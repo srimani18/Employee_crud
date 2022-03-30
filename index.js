d7278a291df933fe352b0f7178f70a35f0bd6610
srimani <37333280+srimani18@users.noreply.github.com>
2022-03-30 12:53:36 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,37 @@
+var express = require('express'),
+   app = express(),
+   port = process.env.PORT || 3000,
+   mongoose = require('mongoose'),
+   cors = require('cors'),
+   bodyParser = require('body-parser'),
+   Employee = require('./models/employee'); //created model loading here
+// Connecting with mongo db
+mongoose.Promise = global.Promise;
+mongoose.connect('mongodb+srv://srimani:Prudhvi16*@cluster0.ccmmr.mongodb.net/empdb?retryWrites=true&w=majority',  { useNewUrlParser: true, useUnifiedTopology: true })
+   .then(() => {
+      console.log('Database sucessfully connected')
+   },
+   error => {
+      console.log('Database could not connected: ' + error)
+   }
+)
+// Setting up configurations
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cors());
+// Setting up the routes
+var employeeRoute = require('./routes/employee'); //importing route
+employeeRoute(app); //register the route
+app.listen(port, () => {
+  console.log('Connected to port ' + port)
+})
+// Find 404 and hand over to error handler
+app.use((req, res, next) => {
+   next(createError(404));
+});
+// error handler
+app.use(function (err, req, res, next) {
+  console.error(err.message); 
+  if (!err.statusCode) err.statusCode = 500; // If err has no specified error code, set error code to 'Internal Server Error (500)'
+  res.status(err.statusCode).send(err.message); // All HTTP requests must have a response, so let's send back an error with its status code and message
+});
